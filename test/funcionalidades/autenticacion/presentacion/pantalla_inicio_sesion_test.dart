9620393f0ee4035658d591969633b0c6c1cac2cd
Hebert Suarez Burgos <suarezburgoshebert@gmail.com>
2026-07-14 21:04:22 -0400

    test(autenticacion): agregar caso de error de red en PantallaInicioSesion

--- test/funcionalidades/autenticacion/presentacion/pantalla_inicio_sesion_test.dart
+++ test/funcionalidades/autenticacion/presentacion/pantalla_inicio_sesion_test.dart
@@ -6,6 +6,7 @@ import 'package:barber_app/funcionalidades/autenticacion/dominio/modelo_barberia
 import 'package:barber_app/funcionalidades/autenticacion/dominio/modelo_perfil.dart';
 import 'package:barber_app/funcionalidades/autenticacion/presentacion/controladores/controlador_autenticacion.dart';
 import 'package:barber_app/funcionalidades/autenticacion/presentacion/pantallas/pantalla_inicio_sesion.dart';
+import 'package:barber_app/nucleo/errores/excepciones_app.dart';
 
 class _RepositorioAutenticacionFalso implements RepositorioAutenticacion {
   bool googleLlamado = false;
@@ -13,6 +14,28 @@ class _RepositorioAutenticacionFalso implements RepositorioAutenticacion {
   @override
   Future<void> iniciarSesionConGoogle() async {
     googleLlamado = true;
+  }
+
+  @override
+  Future<void> iniciarSesionConFacebook() async {}
+
+  @override
+  Future<ModeloPerfil?> obtenerPerfilActual() async => null;
+
+  @override
+  Future<List<ModeloBarberiaResumen>> obtenerBarberiasActivas() async => [];
+
+  @override
+  Future<void> asignarBarberia(String barberiaId) async {}
+
+  @override
+  Future<void> cerrarSesion() async {}
+}
+
+class _RepositorioAutenticacionConErrorDeRed implements RepositorioAutenticacion {
+  @override
+  Future<void> iniciarSesionConGoogle() async {
+    throw const ExcepcionRed();
   }
 
   @override
@@ -50,5 +73,25 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(falso.googleLlamado, isTrue);
+  });
+
+  testWidgets('muestra un SnackBar con el mensaje de error de red al fallar el login de Google', (
+    tester,
+  ) async {
+    final falso = _RepositorioAutenticacionConErrorDeRed();
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [repositorioAutenticacionProvider.overrideWithValue(falso)],
+        child: const MaterialApp(home: PantallaInicioSesion()),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Continuar con Google'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(SnackBar), findsOneWidget);
+    expect(find.text('Error de conexión. Verifica tu internet.'), findsOneWidget);
   });
 }
