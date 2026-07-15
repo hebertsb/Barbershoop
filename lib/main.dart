7ed0d1e31d7a5b3a53fd3a8c890b54a5656435bd
Hebert Suarez Burgos <suarezburgoshebert@gmail.com>
2026-07-14 22:15:32 -0400

    feat: inicializar Google Sign In y usar enrutadorAppProvider en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'nucleo/configuracion/cliente_supabase.dart';
+import 'nucleo/configuracion/constantes.dart';
 import 'nucleo/configuracion/tema_app.dart';
 import 'nucleo/enrutador/enrutador_app.dart';
 
@@ -10,18 +12,20 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await ClienteSupabase.inicializar();
+  await GoogleSignIn.instance.initialize(serverClientId: Constantes.googleWebClientId);
   runApp(const ProviderScope(child: BarberApp()));
 }
 
-class BarberApp extends StatelessWidget {
+class BarberApp extends ConsumerWidget {
   const BarberApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final enrutador = ref.watch(enrutadorAppProvider);
     return MaterialApp.router(
       title: 'BarberApp',
       theme: TemaApp.claro,
-      routerConfig: enrutadorApp,
+      routerConfig: enrutador,
       debugShowCheckedModeBanner: false,
     );
   }
